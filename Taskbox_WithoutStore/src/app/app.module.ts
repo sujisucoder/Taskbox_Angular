fc7f85db2322906bc501dbbb5eedfd3724ca12d4
sujith surendran <sujithsurendran4433@gmail.com>
2023-05-09 00:08:09 +0530

    Added tasklist and models folder

--- Taskbox_WithoutStore/src/app/app.module.ts
+++ Taskbox_WithoutStore/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { MarkdownModule, MarkdownService } from 'ngx-markdown';
 import { HttpClient } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
+import { TaskListComponent } './components/task-list.component';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -21,7 +22,8 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
   declarations: [
     AppComponent,
     TaskComponent,
-    IntroComponent
+    IntroComponent,
+    TaskListComponent
   ],
   exports: [],
   imports: [
---
+++ Taskbox_WithoutStore/src/app/components/task-list.stories.ts
@@ -0,0 +1,120 @@
+
+import { componentWrapperDecorator, moduleMetadata } from '@storybook/angular';
+import type { Meta, StoryObj } from '@storybook/angular/';
+
+import { CommonModule } from '@angular/common';
+
+import { TaskListComponent } from './task-list.component';
+import { TaskComponent } from './task.component';
+import { action } from '@storybook/addon-actions';
+
+import * as TaskStories from './task.stories';
+import { Task } from '../models/task.model';
+
+const meta: Meta<TaskListComponent> = {
+    /* 👇 The title prop is optional.
+     * See https://storybook.js.org/docs/angular/configure/overview#configure-story-loading
+     * to learn how to generate automatic titles
+     */
+    title: 'TaskListComponent',
+    component: TaskListComponent,
+    tags: ['autodocs'],
+    decorators: [
+        moduleMetadata({
+            //👇 Imports both components to allow component composition with Storybook
+            declarations: [TaskListComponent, TaskComponent],
+            imports: [CommonModule],
+        }),
+        //👇 Wraps our stories with a decorator
+        // componentWrapperDecorator(story => `<div style="margin: 3em">${story}</div>`),
+        componentWrapperDecorator(
+            (story) => `<div style="margin: 3em">${story}</div>`
+          ),
+    ],
+};
+
+export default meta;
+type Story = StoryObj<TaskListComponent>;
+
+const actionsData = {
+    onPinTask: action('onPinTask'),
+    onArchiveTask: action('onArchiveTask'),
+};
+
+
+export const Template: Story = {
+    args: {
+        onPinTask: TaskStories.Default.args?.onPinTask,
+        onArchiveTask: TaskStories.Default.args?.onArchiveTask,
+      }
+}
+
+export const DefaultList: Story = {
+    args: {
+        tasks: [
+          {
+            ...(TaskStories.Default.args?.['task'] as Task),
+            id: '1',
+            title: 'Task 1',
+          },
+          {
+            ...(TaskStories.Default.args?.['task'] as Task),
+            id: '2',
+            title: 'Task 2',
+          },
+          {
+            ...(TaskStories.Default.args?.['task'] as Task),
+            id: '3',
+            title: 'Task 3',
+          },
+          {
+            ...(TaskStories.Default.args?.['task'] as Task),
+            id: '4',
+            title: 'Task 4',
+          },
+          {
+            ...(TaskStories.Default.args?.['task'] as Task),
+            id: '5',
+            title: 'Task 5',
+          },
+          {
+            ...(TaskStories.Default.args?.['task'] as Task),
+            id: '6',
+            title: 'Task 6',
+          },
+        ],
+      },
+};
+
+  
+export const WithPinnedTasks: Story = {
+
+args:{
+    tasks: [
+        ...(DefaultList.args?.['tasks'] as Task[] ).slice(0, 5),
+        { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
+    ],
+
+}
+}
+
+export const Loading: Story ={
+    args : {
+        tasks: [],
+        loading: true,
+    }
+
+}
+
+export const Empty: Story = {
+   args : {
+        // Shaping the stories through args composition.
+        // Inherited data coming from the Loading story.
+        // ...Loading.args,
+        ...Loading.args,
+        loading: false,
+    }
+    
+
+}
+
